7130a6c414f86b5f85d9a7e0910ba96c40cb002b
Greenhouse89 <greenbrandon07@yahoo.com>
2021-10-07 09:24:05 -0400

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello {
+    //public is access modifier 
+    //static is Java keyword that for java to find method and run
+    //void is keyword that indicates method returns nothing
+    //perameters pass info into method
+    public static void main (String[] args){
+        System.out.println("Hello Brandon");
+
+        int myFirstNumber = 5;
+    }
+}
