c01a4b659b4d2c1919601728ddf1d3507eb22bc9
Greenhouse89 <greenbrandon07@yahoo.com>
2021-10-07 09:37:57 -0400

    added mylastone with op

--- src/Hello.java
+++ src/Hello.java
@@ -8,11 +8,12 @@ public class Hello {
 
         int myFirstNumber = (10 + 5 ) + (2 + 10);
         int mySecondNumber = 12;
-        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber *2;
         int myTotal = myFirstNumber + mySecondNumber+ myThirdNumber;
         System.out.println(myTotal);
 
-   
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
         
 
         
