ab7a9de805adfc394764819d5f93de9a25a70426
Greenhouse89 <greenbrandon07@yahoo.com>
2021-10-07 09:28:58 -0400

    added first number with operators

--- src/Hello.java
+++ src/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
     public static void main (String[] args){
         System.out.println("Hello Brandon");
 
-        int myFirstNumber = 5;
+        int myFirstNumber = (10 + 5 ) + (2 + 10);
+        System.out.println(myFirstNumber);
     }
 }
